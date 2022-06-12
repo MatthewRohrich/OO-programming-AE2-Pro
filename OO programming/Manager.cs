55ada9d5764bcc72c66dc4098415fdf50a672fbe
MatthewRohrich <58925243+MatthewRohrich@users.noreply.github.com>
2022-06-12 20:45:57 +1000

    update manager class xml

--- OO programming/Manager.cs
+++ OO programming/Manager.cs
@@ -8,7 +8,15 @@
         // permissions to control timesheet approval
         private string permissions;
 
-        // Manager constructor inherits from Employee
+        /// <summary>
+        /// Manager constructor inherits from Employee
+        /// </summary>
+        /// <param name="employeeId"></param>
+        /// <param name="firstName"></param>
+        /// <param name="lastName"></param>
+        /// <param name="hrlyRate"></param>
+        /// <param name="taxWithThresholdFlag"></param>
+        /// <param name="permissions"></param>
         public Manager(int employeeId, string firstName, string lastName, int hrlyRate, string taxWithThresholdFlag, string permissions) : base(employeeId, firstName, lastName, hrlyRate, taxWithThresholdFlag)
         {
             this.EmployeeId = employeeId;
@@ -20,7 +28,11 @@
 
         }
 
-
+        /// <summary>
+        /// Permissions of the manager in the pay system.
+        /// permissions are currently only a placeholder and
+        /// not implemented
+        /// </summary>
         public string Permissions { get => permissions; set => permissions = value; }
     }
 }
